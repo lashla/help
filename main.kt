a880a9a118c3161d4a0fef983f48a19ff0407eb0
lashla <45652713+lashla@users.noreply.github.com>
2022-01-14 15:05:54 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,1 +1,21 @@
+fun multiplyByTwo(number: Int): Int {
+    return number * 2
+}
 
+fun addTen(number: Int): Int {
+    return number + 10
+}
+
+fun changeNumber(changeFunction: (Int) -> Int, number: Int) {
+    val newNumber = changeFunction(number)
+    print("$newNumber ")
+}
+
+fun main() {
+    val numbers = readLine()!!.split(' ').map { it.toInt() }.toList()
+    var numberFun: (Int) -> Int = if (this % 2 != 0) ::multiplyByTwo else ::addTen // вот в этой строке крч надо например Int из скобок использовать вместо this
+    for (number in numbers) {
+        
+        changeNumber(numberFun, number)
+    }
+}
